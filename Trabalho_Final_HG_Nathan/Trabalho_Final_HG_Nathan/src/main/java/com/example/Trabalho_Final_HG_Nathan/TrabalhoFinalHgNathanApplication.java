a17c44649768c9b518941650f9c416c4c9ad15ce
Henrique <henriquegbr2004@gmail.com>
2025-11-27 21:25:17 -0300

    Add files via upload
    Esqueleto basico para o trabalho, mvc

---
+++ Trabalho_Final_HG_Nathan/Trabalho_Final_HG_Nathan/src/main/java/com/example/Trabalho_Final_HG_Nathan/TrabalhoFinalHgNathanApplication.java
@@ -0,0 +1,13 @@
+package com.example.Trabalho_Final_HG_Nathan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalHgNathanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalHgNathanApplication.class, args);
+	}
+
+}
---
+++ Trabalho_Final_HG_Nathan/Trabalho_Final_HG_Nathan/src/test/java/com/example/Trabalho_Final_HG_Nathan/TrabalhoFinalHgNathanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trabalho_Final_HG_Nathan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalHgNathanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
