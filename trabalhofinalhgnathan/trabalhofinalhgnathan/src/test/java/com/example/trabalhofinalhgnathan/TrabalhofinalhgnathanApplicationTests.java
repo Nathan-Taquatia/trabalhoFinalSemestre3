e399945a94ba0ec2cf724ba5e6d8fbf4a872431c
Henrique <henriquegbr2004@gmail.com>
2025-12-06 20:24:18 -0300

    trabalho final v2
    nomes corretos da pasta principal (e model)

---
+++ trabalhofinalhgnathan/trabalhofinalhgnathan/src/test/java/com/example/trabalhofinalhgnathan/TrabalhofinalhgnathanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabalhofinalhgnathan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhofinalhgnathanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
